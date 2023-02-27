3921b1048a8a3c754dabc61ba7899cf0c777ee62
Arjun31415 <arjunp0710@gmail.com>
2023-02-27 21:04:35 +0530

    updated docs

--- docs/html/search/functions_2.js
+++ docs/html/search/functions_2.js
@@ -1,9 +1,11 @@
 var searchData=
 [
-  ['gaussian_5fblur_0',['gaussian_blur',['../blur_8cu.html#a49a19600bbc0b45e4b8f000d474324c1',1,'gaussian_blur(const float *kernel, int n, const cv::cuda::PtrStepSz&lt; T_in &gt; input, cv::cuda::PtrStepSz&lt; T_out &gt; output):&#160;blur.cu'],['../blur_8cu.html#a55f119e995b3421b7616c68bb14e6429',1,'gaussian_blur(const cv::Mat &amp;input, cv::Mat &amp;output, const int n=3, const float sigma=1.0, bool two_d=true):&#160;blur.cu']]],
-  ['gaussian_5fblur_5fexit_1',['gaussian_blur_exit',['../blur_8cu.html#a76d70a60a6f796eb879606af2c1f4fb7',1,'blur.cu']]],
+  ['gaussian_5fblur_0',['gaussian_blur',['../blur_8cu.html#a49a19600bbc0b45e4b8f000d474324c1',1,'gaussian_blur(const float *kernel, int n, const cv::cuda::PtrStepSz&lt; T_in &gt; input, cv::cuda::PtrStepSz&lt; T_out &gt; output):&#160;blur.cu'],['../blur_8cu.html#ab98e5922cccda7c107a1a51493d97bd1',1,'gaussian_blur(const cv::Mat &amp;input, cv::Mat &amp;output, const int n=3, const float sigma=1.0, bool two_d=true, bool remove_globals=true):&#160;blur.cu']]],
+  ['gaussian_5fblur_5fexit_1',['gaussian_blur_exit',['../blur_8cu.html#aedfd621486f0d15a75a7e27662dc7c3d',1,'blur.cu']]],
   ['gaussian_5fblur_5finit_2',['gaussian_blur_init',['../blur_8cu.html#a74fff5eb6cbf35b2126ed8286e65214c',1,'blur.cu']]],
-  ['generate_5fgaussian_5fkernel_3',['generate_gaussian_kernel',['../main_8cpp.html#aee6c26bcb89a471d6240f9b3186defde',1,'main.cpp']]],
-  ['generate_5fgaussian_5fkernel_5f1d_4',['generate_gaussian_kernel_1d',['../blur_8cu.html#a53361b59233e810079be92f55e114c97',1,'blur.cu']]],
-  ['generate_5fgaussian_5fkernel_5f2d_5',['generate_gaussian_kernel_2d',['../blur_8cu.html#a0019adbb68eaf5f0c5176bbdf37ffcf1',1,'blur.cu']]]
+  ['gaussian_5fblur_5fx_3',['gaussian_blur_x',['../blur_8cu.html#a8687a4eee31a74468570db30a29a31c9',1,'blur.cu']]],
+  ['gaussian_5fblur_5fy_4',['gaussian_blur_y',['../blur_8cu.html#a411df053a2d5e063133ed6c21a0cbd0c',1,'blur.cu']]],
+  ['generate_5fgaussian_5fkernel_5',['generate_gaussian_kernel',['../main_8cpp.html#aee6c26bcb89a471d6240f9b3186defde',1,'main.cpp']]],
+  ['generate_5fgaussian_5fkernel_5f1d_6',['generate_gaussian_kernel_1d',['../blur_8cu.html#a53361b59233e810079be92f55e114c97',1,'blur.cu']]],
+  ['generate_5fgaussian_5fkernel_5f2d_7',['generate_gaussian_kernel_2d',['../blur_8cu.html#a0019adbb68eaf5f0c5176bbdf37ffcf1',1,'blur.cu']]]
 ];
